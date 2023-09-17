745f4c94f47f78f3c4d2c99009d5645dc2567e37
Stanislav-Zhuk <steys.ua@gmail.com>
2023-09-17 15:24:06 +0300

    update Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,45 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import carImage from '../../assets/audi.png';
+import css from './Home.module.css';
 
 const Home = () => {
   return (
-    <div>
-      <header>
-        <h1>Your Journey Begins Here with AutoRent</h1>
-      </header>
-      <main>
-        <section>
-          <h2>Our services</h2>
-          <p>
-            We provide a wide selection of cars for rent in Ukraine. Regardless
-            from your budget and needs, we have a car for you.
-          </p>
-        </section>
-        <section>
-          <h2>About us</h2>
-          <p>
-            Our company specializes in providing cars for rent with 10 years of
-            experience. We proudly serve clients throughout Ukraine.
-          </p>
-        </section>
-      </main>
-      <footer>
-        <p>&copy; 2023 Car rental in Ukraine</p>
-      </footer>
-    </div>
+    <main className={css.container}>
+      <img src={carImage} className={css.carImage} alt="Car" />
+      <h1 className={css.heroTitle}>
+        Your Journey Begins Here with{' '}
+        <span className={css.accent}>AutoRent</span>
+      </h1>
+      <section>
+        <h2 className={css.sectionTitle}>Our services</h2>
+        <p className={css.sectionText}>
+          We offer a wide range of rental cars in Ukraine. Regardless of your
+          budget and needs, we have a car that meets your requirements. We
+          provide cars of various classes and models to cater to all your
+          mobility needs. Our goal is to make your journey comfortable and safe
+          by giving you access to high-quality vehicles at affordable prices.
+          Make your choice according to your budget and enjoy your trip with us.
+        </p>
+      </section>
+      <section>
+        <h2 className={css.sectionTitle}>About us</h2>
+        <p className={css.sectionText}>
+          Our company specializes in providing cars for rent and has a decade of
+          experience in the field. We take pride in serving clients across the
+          entire Ukraine, offering a wide range of vehicles to meet various
+          mobility needs. Our mission is to ensure your journey is not only
+          comfortable but also unforgettable. With a decade of expertise under
+          our belt, we are committed to delivering the best rental experience
+          possible.
+        </p>
+      </section>
+      <Link to={'/catalog'} className="button-primary button-home">
+        Book now
+      </Link>
+      <p className={css.copyright}>
+        &copy; 2023 AutoRent. Created by Stanislav Zhuk.
+      </p>
+    </main>
   );
 };
 
